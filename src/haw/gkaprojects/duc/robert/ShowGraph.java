cb3280796f1c77acd0108a8687b357889017a189
Robert <rscheffel00@gmail.com>
2015-04-25 09:56:13 +0200

    GUI Choose Graph Menu

--- src/haw/gkaprojects/duc/robert/ShowGraph.java
+++ src/haw/gkaprojects/duc/robert/ShowGraph.java
@@ -8,6 +8,7 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.LayoutManager;
@@ -17,6 +18,7 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
@@ -36,43 +38,91 @@ import com.jgraph.layout.hierarchical.JGraphLongestPathLayering;
 
 public class ShowGraph
 {
-	
+
 	JFrame _frame;
 	FileReader _reader;
 
 	Graph<Vertex, CustomEdge> _jGraphT;
-	
-	public ShowGraph(){
+
+	public ShowGraph()
+	{
 		_frame = new JFrame();
 		_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		_frame.setBounds(0, 0, 1200, 800);
 		_frame.setLocationRelativeTo(null);
-		
+
 		JMenuBar mb = new JMenuBar();
 		JMenu menu = new JMenu("Menue");
 		JMenuItem miOpenFile = new JMenuItem("Open File");
+		JMenu menuAl = new JMenu("Algrorithmus");
+
+		JMenu bfs = new JMenu("bfs");
+
+		JPanel searchPanel = new JPanel();
+		searchPanel.setLayout(new FlowLayout());
+
+		JPanel startPointPanel = new JPanel();
+		JLabel startPoint = new JLabel("Waehle einen Startpunkt!");
+		String[] a =
+		{ "Lade einen Graphen!" };
+		JComboBox<String> boxStart = new JComboBox<>(a);
+		boxStart.addActionListener(new ActionListener()
+		{
+
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+
+			}
+		});
+		startPointPanel.add(startPoint);
+		startPointPanel.add(boxStart);
+
+		JPanel endPointPanel = new JPanel();
+		JLabel endPoint = new JLabel("Waehle einen Zielpunkt!");
+
+		JComboBox<String> boxEnd = new JComboBox<>(a);
+		startPointPanel.add(endPoint);
+		startPointPanel.add(boxEnd);
+
+		JButton buttonFind = new JButton("Search");
+
+		searchPanel.add(startPointPanel);
+		searchPanel.add(endPointPanel);
+		searchPanel.add(buttonFind);
+
+		bfs.add(searchPanel);
+
+		JMenu AStern = new JMenu("AStern");
+
+		JMenu fWay = new JMenu("Find Way");
+
+		menuAl.add(bfs);
+		menuAl.add(AStern);
+
 		miOpenFile.addActionListener(new ActionListener()
 		{
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-			openFilePopUp();
+				openFilePopUp(boxStart, boxEnd);
 			}
 		});
+
 		JMenuItem miSaveFile = new JMenuItem("Save File");
 		miSaveFile.addActionListener(new ActionListener()
 		{
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				GraphFileSaver.saveGraphToFile( "G:\\Git\\GKA\\result", _jGraphT);
+				GraphFileSaver
+						.saveGraphToFile("G:\\Git\\GKA\\result", _jGraphT);
 			}
 
-
 		});
-		
+
 		JMenuItem miExit = new JMenuItem("Exit");
 		miExit.addActionListener(new ActionListener()
 		{
@@ -82,131 +132,130 @@ public class ShowGraph
 				_frame.dispose();
 			}
 		});
-		
-		
+
 		menu.add(miOpenFile);
 		menu.add(miSaveFile);
 		menu.add(miExit);
-		
-		JMenu menuAl = new JMenu("Algrorithmus");
-		
-		JMenu bfs = new JMenu("bfs");
-		
-		String[] a = {"Lade einen Graphen!"};
-		JComboBox<String> box = new JComboBox<>(a);
-		
-		bfs.add(box);
-		
-		JMenu fWay = new JMenu("Find Way");
-		
-		menuAl.add(bfs);
-		
+
 		mb.add(menu);
 		mb.add(menuAl);
 		mb.add(fWay);
-		
-		_frame.setJMenuBar(mb);		
-		
-//		_frame.pack();
+
+		_frame.setJMenuBar(mb);
+
+		// _frame.pack();
 		_frame.setVisible(true);
-		
 
 	}
-	
+
 	private String[] vertexSetToString()
 	{
 		String[] a = new String[_jGraphT.vertexSet().size()];
-		
-		for( Vertex v :_jGraphT.vertexSet())
+
+		for (Vertex v : _jGraphT.vertexSet())
 		{
 			v.toString();
 		}
 		return a;
 	}
 
-	public void setGraph(JGraph graph){
+	public void setGraph(JGraph graph)
+	{
 		_frame.getContentPane().add(graph);
 		update();
-		
+
 	}
-	
-	private void openFilePopUp()
+
+	private void openFilePopUp(JComboBox<String> boxStart,
+			JComboBox<String> boxEnd)
 	{
 		System.out.println("klicked");
 		JFrame popup = new JFrame("Choose File");
 		popup.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		Container contentPane = popup.getContentPane();
-		
-		
-	    final JLabel directoryLabel = new JLabel(" ");
-	    directoryLabel.setFont(new Font("Serif", Font.BOLD | Font.ITALIC, 36));
-	    contentPane.add(directoryLabel, BorderLayout.NORTH);
 
-//	    final JLabel filenameLabel = new JLabel(" ");
-//	    filenameLabel.setFont(new Font("Serif", Font.BOLD | Font.ITALIC, 36));
-//	    contentPane.add(filenameLabel, BorderLayout.SOUTH);
-	    
-	    final JButton open = new JButton("Open File");
-	    contentPane.add(open, BorderLayout.SOUTH);
+		final JLabel directoryLabel = new JLabel(" ");
+		directoryLabel.setFont(new Font("Serif", Font.BOLD | Font.ITALIC, 36));
+		contentPane.add(directoryLabel, BorderLayout.NORTH);
 
-	    JFileChooser fileChooser = new JFileChooser("../GKA/res/files/bspGraphen/");
-	    fileChooser.setControlButtonsAreShown(false);
-	    contentPane.add(fileChooser, BorderLayout.CENTER);
+		// final JLabel filenameLabel = new JLabel(" ");
+		// filenameLabel.setFont(new Font("Serif", Font.BOLD | Font.ITALIC,
+		// 36));
+		// contentPane.add(filenameLabel, BorderLayout.SOUTH);
 
-	    ActionListener actionListener = new ActionListener() {
-	      public void actionPerformed(ActionEvent actionEvent) {
-	        JFileChooser theFileChooser = (JFileChooser) actionEvent
-	            .getSource();
-	        String command = actionEvent.getActionCommand();
-	        if (command.equals(JFileChooser.APPROVE_SELECTION)) {
-	        	_frame.getContentPane().removeAll();    //Test
-	          File selectedFile = theFileChooser.getSelectedFile();
-	          directoryLabel.setText(selectedFile.getParent());
-	          _reader = new FileReader();
-	          try
+		final JButton open = new JButton("Open File");
+		contentPane.add(open, BorderLayout.SOUTH);
+
+		JFileChooser fileChooser = new JFileChooser(
+				"../GKA/res/files/bspGraphen/");
+		fileChooser.setControlButtonsAreShown(false);
+		contentPane.add(fileChooser, BorderLayout.CENTER);
+
+		ActionListener actionListener = new ActionListener()
+		{
+			public void actionPerformed(ActionEvent actionEvent)
 			{
-				_reader.read(selectedFile);
-			}
-			catch (IOException e)
-			{
-//				_frame.getContentPane().add("This File could not be loaded!");
-				e.printStackTrace();
-			}
-	  		CreateGraph createGraph = new CreateGraph(_reader.getRowList());
-			_jGraphT = createGraph.getGraph();
-			JGraph _jgraph = new JGraph(new JGraphModelAdapter<>(_jGraphT));
-	
-			setGraph(_jgraph);
-			
-			popup.dispose();
-			
-	          System.out.println(selectedFile);
+				JFileChooser theFileChooser = (JFileChooser) actionEvent
+						.getSource();
+				String command = actionEvent.getActionCommand();
+				if (command.equals(JFileChooser.APPROVE_SELECTION))
+				{
+					_frame.getContentPane().removeAll(); // Test
+					File selectedFile = theFileChooser.getSelectedFile();
+					directoryLabel.setText(selectedFile.getParent());
+					_reader = new FileReader();
+					try
+					{
+						_reader.read(selectedFile);
+					}
+					catch (IOException e)
+					{
+						_frame.getContentPane().add(
+								new JLabel("This File could not be loaded!"));
+						e.printStackTrace();
+					}
+					CreateGraph createGraph = new CreateGraph(
+							_reader.getRowList());
+					_jGraphT = createGraph.getGraph();
+					JGraph _jgraph = new JGraph(new JGraphModelAdapter<>(
+							_jGraphT));
+					for (Vertex v : _jGraphT.vertexSet())
+					{
+						boxStart.addItem(v.getLabel());
+						boxEnd.addItem(v.getLabel());
+						
+					}
+					setGraph(_jgraph);
 
-//	         filenameLabel.setText(selectedFile.getName());
-	        } else if (command.equals(JFileChooser.CANCEL_SELECTION)) {
-	          directoryLabel.setText(" ");
-	      
-//	          filenameLabel.setText(" ");
-	        }
-	      }
-	    };
-	    
-	    
-	    fileChooser.addActionListener(actionListener);
-	   
+					popup.dispose();
 
-	    popup.pack();
-	    popup.setVisible(true);
+					System.out.println(selectedFile);
+
+					// filenameLabel.setText(selectedFile.getName());
+				}
+				else if (command.equals(JFileChooser.CANCEL_SELECTION))
+				{
+					directoryLabel.setText(" ");
+
+					// filenameLabel.setText(" ");
+				}
+			}
+		};
+
+		fileChooser.addActionListener(actionListener);
+
+		popup.pack();
+		popup.setVisible(true);
 	}
-	
+
 	/*
 	 * updates the contentpane of the jframe
 	 */
 	private void update()
 	{
-	_frame.getContentPane().repaint();
-	_frame.setVisible(true);
-	System.out.println("tata");
-		
+		_frame.getContentPane().repaint();
+		_frame.setVisible(true);
+		System.out.println("tata");
+
 	}
 }
