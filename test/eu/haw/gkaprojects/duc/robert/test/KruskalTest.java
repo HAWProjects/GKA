77ae1a671cd7076f16ba4090596cf5a7d30cbb22
Robert <rscheffel00@gmail.com>
2015-05-17 16:22:36 +0200

    another test

--- test/eu/haw/gkaprojects/duc/robert/test/KruskalTest.java
+++ test/eu/haw/gkaprojects/duc/robert/test/KruskalTest.java
@@ -15,20 +15,32 @@ import org.junit.Test;
 public class KruskalTest
 {
 	private static final String PATH1 = "../GKA/test/eu/haw/gkaprojects/duc/robert/test/test/TestfileKruskal.graph";
+	private static final String PATH4 = "../GKA/res/files/bspGraphen/bsp4.graph";
 	private Graph<Vertex, CustomEdge> _graph1;
+	private Graph<Vertex, CustomEdge> _graph4;
+	
 	@Before
 	public void setUp() throws Exception {
 		File file1 = new File(PATH1);
+		File file4 = new File(PATH4);
 		GraphMaker_withScanner scanner3 = new GraphMaker_withScanner(file1);
+		GraphMaker_withScanner scanner4 = new GraphMaker_withScanner(file4);
 		 _graph1 = scanner3.getGraph();
-		 System.out.println(_graph1);
+		 _graph4 = scanner4.getGraph();
+		 
 	}
 	@Test
-	public void testTruskal()
+	public void testTruskal1()
 	{
 		Kruskal krusk = new Kruskal(_graph1);
-	assertEquals(4, krusk.getSpanningTree().edgeSet().size());	
+		assertEquals(4, krusk.getSpanningTree().edgeSet().size());	
+	}
 	
+	@Test
+	public void testTruskal2()
+	{
+		Kruskal krusk = new Kruskal(_graph4);
+		assertEquals(10, krusk.getSpanningTree().edgeSet().size());	
 	}
 
 }
