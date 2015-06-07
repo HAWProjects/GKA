fb12696eab9aac0c81c1c69b7498793e019bd17d
mducnguyen <m.duc.nguyen.93@gmail.com>
2015-06-07 15:38:05 +0200

    Method isEulerianCircuit implemented

--- test/eu/haw/gkaprojects/duc/robert/test/CheckCurcuitCorrectness.java
+++ test/eu/haw/gkaprojects/duc/robert/test/CheckCurcuitCorrectness.java
@@ -1,9 +1,5 @@
 package eu.haw.gkaprojects.duc.robert.test;
 
-import haw.gkaprojects.duc.robert.graph.CustomEdge;
-import haw.gkaprojects.duc.robert.graph.Vertex;
-
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -12,50 +8,40 @@ import org.jgrapht.UndirectedGraph;
 
 public class CheckCurcuitCorrectness {
      
-      public  boolean isEulerCircuit(UndirectedGraph<Vertex, CustomEdge> graph, List<CustomEdge> circuit) {
+      public static <V,E> boolean isEulerianCircuit(UndirectedGraph<V,E> graph, List<E> circuit) {
             
             
-            Set<CustomEdge> circuitSet = new HashSet<CustomEdge>(circuit);
+            Set<V> s = new HashSet<V>();
             
-            if(circuitSet.size() != graph.edgeSet().size()){
+            if(s.size() != graph.edgeSet().size()){
                   return false;
             }
             
-            if (circuit.size() == 1) {
-                  return true;
+            for(int i=0; i<circuit.size(); i++){
+                  E edge = circuit.get(i);
+                  
+                  V v1 = graph.getEdgeSource(edge);
+                  V v2 = graph.getEdgeTarget(edge);
+                  
+                  if(!circuit.contains(v1)){
+                        s.add(v1);
+                  }else {
+                        s.remove(v1);
+                  }
+                  
+                  if(!circuit.contains(v2)){
+                        s.add(v2);
+                  }else {
+                        s.remove(v2);
+                  }
+                 
             }
             
-            Vertex  start = findStart(graph, circuit);
-            Vertex end   = findEnd(graph, circuit);
             
-//            if(iSstartAndEndVertexMatch())
-            
-            
-            return false;
+            return s.isEmpty();
             
       }
 
-      private Vertex findEnd(UndirectedGraph<Vertex, CustomEdge> graph, List<CustomEdge> circuit) {
-            
-            CustomEdge first = circuit.get(0);
-            CustomEdge second = circuit.get(1);
-            
-            List<Vertex> l = new ArrayList<Vertex>();
-            
-            l.add(graph.getEdgeSource(first));
-            l.add(graph.getEdgeTarget(first));
-            
-            l.remove(graph.getEdgeSource(second));
-            l.remove(graph.getEdgeTarget(second));
-            
-            Vertex start = l.get(0);
-            
-            return start;
-      }
-
-      private Vertex findStart(UndirectedGraph<Vertex, CustomEdge> graph, List<CustomEdge> circuit) {
-            // TODO Auto-generated method stub
-            return null;
-      }
+      
 
 }
