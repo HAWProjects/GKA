db24984b120fa3c431f81f5cf0b18825655dbd8e
Robert <rscheffel00@gmail.com>
2015-06-14 13:32:08 +0200

    add another test

--- test/eu/haw/gkaprojects/duc/robert/test/algo/FleuryTest.java
+++ test/eu/haw/gkaprojects/duc/robert/test/algo/FleuryTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import haw.gkaprojects.duc.robert.UndirectedEulerianGraphConstructor;
 import haw.gkaprojects.duc.robert.EulerianCircuit.EulerUtil;
 import haw.gkaprojects.duc.robert.EulerianCircuit.FleuryEulerian;
+import haw.gkaprojects.duc.robert.EulerianCircuit.HierholzerEulerianCircuit;
 import haw.gkaprojects.duc.robert.graph.CustomEdge;
 import haw.gkaprojects.duc.robert.graph.Vertex;
 
@@ -13,67 +14,94 @@ import java.util.Random;
 import org.jgrapht.UndirectedGraph;
 import org.junit.Test;
 
-public class FleuryTest {
+public class FleuryTest
+{
 	
-    private int testcount      = 200;
-    private int minVertexcount = 50;
-    
+	private int testcount = 200;
+	private int minVertexcount = 50;
+	
 	@Test
-	public void testSmallGraph() {
-       
-		int verticesAmount = 15;
-        int edgesAmount = 30;
-        for (int i = 0; i < testcount; i++) {
-        	
-            Random rand = new Random();
-
-            do {
-
-                  verticesAmount = rand.nextInt(100) + minVertexcount;
-                  edgesAmount = rand.nextInt(300);
-            }while(!UndirectedEulerianGraphConstructor.isArgumentStatisfied(verticesAmount,
-                    edgesAmount));
-            	
-            UndirectedGraph<Vertex, CustomEdge> graph = (UndirectedGraph<Vertex, CustomEdge>) UndirectedEulerianGraphConstructor
-                    .constructGraph(verticesAmount, edgesAmount);
-            FleuryEulerian<Vertex, CustomEdge> eulerGraph = new FleuryEulerian<Vertex,CustomEdge>(graph);
-            List<CustomEdge> circuit = eulerGraph.getEulerianCircuit();
-            if(EulerUtil.isEulerianGraph(graph)){
-            	assertTrue(EulerUtil.isEulerianCircuit(graph, circuit));
-            }else{
-            	 fail("No Euler Circuit");
-            }
-            	
-            }
-        }
+	public void testSmallGraph()
+	{
 		
-	
-	
+		int verticesAmount = 15;
+		int edgesAmount = 30;
+		for(int i = 0; i < testcount; i++)
+		{
+			
+			Random rand = new Random();
+			
+			do
+			{
+				
+				verticesAmount = rand.nextInt(100) + minVertexcount;
+				edgesAmount = rand.nextInt(300);
+			}
+			while(!UndirectedEulerianGraphConstructor.isArgumentStatisfied(verticesAmount, edgesAmount));
+			
+			UndirectedGraph<Vertex, CustomEdge> graph = (UndirectedGraph<Vertex, CustomEdge>) UndirectedEulerianGraphConstructor.constructGraph(
+					verticesAmount, edgesAmount);
+			FleuryEulerian<Vertex, CustomEdge> eulerGraph = new FleuryEulerian<Vertex, CustomEdge>(graph);
+			List<CustomEdge> circuit = eulerGraph.getEulerianCircuit();
+			assertTrue(EulerUtil.isEulerianCircuit(graph, circuit));
+			
+		}
+	}
 	
 	@Test
-	public void testRandomizedEulerianGraph() {
-      
-		int verticesAmount = 0;
-        int edgesAmount = 0;
-        for (int i = 0; i < testcount; i++) {
-            Random rand = new Random();
-            do {
-                  verticesAmount = rand.nextInt(10000) + minVertexcount;
-                  edgesAmount = rand.nextInt(30000);
-
-            } while (!UndirectedEulerianGraphConstructor.isArgumentStatisfied(verticesAmount,
-                        edgesAmount));
-            
-            UndirectedGraph<Vertex, CustomEdge> graph = (UndirectedGraph<Vertex, CustomEdge>) UndirectedEulerianGraphConstructor
-                    .constructGraph(verticesAmount, edgesAmount);
-            FleuryEulerian<Vertex, CustomEdge> eulerGraph = new FleuryEulerian<Vertex,CustomEdge>(graph);
-            List<CustomEdge> circuit = eulerGraph.getEulerianCircuit();
-            if(EulerUtil.isEulerianGraph(graph)){
-            	assertTrue(EulerUtil.isEulerianCircuit(graph, circuit));
-            }else{
-            	 fail("No Euler Circuit");
-            }
-        }
+	public void testSmallGraphFleuryVSHierholzer()
+	{
+		
+		int verticesAmount = 15;
+		int edgesAmount = 30;
+		for(int i = 0; i < 100; i++)
+		{
+			Random rand = new Random();
+			
+			do
+			{
+				verticesAmount = rand.nextInt(50) + minVertexcount;
+				edgesAmount = rand.nextInt(150);
+			}
+			while(!UndirectedEulerianGraphConstructor.isArgumentStatisfied(verticesAmount, edgesAmount));
+			
+			UndirectedGraph<Vertex, CustomEdge> graph = (UndirectedGraph<Vertex, CustomEdge>) UndirectedEulerianGraphConstructor.constructGraph(
+					verticesAmount, edgesAmount);
+			
+			FleuryEulerian<Vertex, CustomEdge> eulerGraph = new FleuryEulerian<Vertex, CustomEdge>(graph);
+			List<CustomEdge> circuit = eulerGraph.getEulerianCircuit();
+			
+			HierholzerEulerianCircuit<Vertex, CustomEdge> hhgraph = new HierholzerEulerianCircuit<Vertex, CustomEdge>(graph);
+			List<CustomEdge> hhCircuit = hhgraph.getEulerianCircuit();
+			
+			assertEquals(hhCircuit.size(), circuit.size());
+		}
 	}
-
+	
+	@Test
+	public void testRandomizedEulerianGraph()
+	{
+		int verticesAmount = 0;
+		int edgesAmount = 0;
+		for(int i = 0; i < 50; i++)
+		{
+			Random rand = new Random();
+			do
+			{
+				verticesAmount = rand.nextInt(500) + minVertexcount;
+				edgesAmount = rand.nextInt(1500);
+				
+			}
+			while(!UndirectedEulerianGraphConstructor.isArgumentStatisfied(verticesAmount, edgesAmount));
+			
+			UndirectedGraph<Vertex, CustomEdge> graph = (UndirectedGraph<Vertex, CustomEdge>) UndirectedEulerianGraphConstructor.constructGraph(
+					verticesAmount, edgesAmount);
+			FleuryEulerian<Vertex, CustomEdge> eulerGraph = new FleuryEulerian<Vertex, CustomEdge>(graph);
+			List<CustomEdge> circuit = eulerGraph.getEulerianCircuit();
+			
+			assertTrue(EulerUtil.isEulerianCircuit(graph, circuit));
+			
+		}
+	}
+	
 }
